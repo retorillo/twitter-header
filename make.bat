f00aa4899f6e2ebbbea68566c7e1329663faf966
Retorillo <retorillo@outlook.com>
2018-08-03 02:05:35 +0900

    Fix padding size

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 cl /EHsc /Zi /Gm decorator.cpp
 cl /EHsc /Zi /Gm divider.cpp
-SET PADH=12
-SET PADV=6
+SET PADH=6
+SET PADV=12
 figlet -w=160 Welcome to Twitter Land > file1.txt
 type file1.txt | decorator -m 1 -l %PADH% -r %PADH% -t %PADV% -b %PADV% > file2.txt
 type file1.txt | decorator -m 2 -l %PADH% -r %PADH% -t %PADV% -b %PADV% > file3.txt
